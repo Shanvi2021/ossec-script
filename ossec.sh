d30ef73b2c3e00d0168d67bd179ce5166b21c35f
Shanvi2021 <100283082+Shanvi2021@users.noreply.github.com>
2022-06-21 20:43:19 +0530

    Update ossec.sh

--- ossec.sh
+++ ossec.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
+ossecAgent() {
+sudo yum install unzip
+sudo yum install gcc-c++ make -y
+sudo yum install wget
+sudo yum install -y php-cli php-common sendmail inotify-tools
+cd /opt
+ossec_server_ip=”1.1.1.1"
+export VER=”3.1.0"
+sudo wget https://github.com/ossec/ossec-hids/archive/${VER}.tar.gz
+sudo tar -xvzf /opt/${VER}.tar.gz
+cd /opt/ossec-hids-${VER}
+sudo sed -i ‘2 i USER_LANGUAGE=$1’ install.sh
+sudo sed -i ‘2 i USER_INSTALL_TYPE=$2’ install.sh
+sudo sed -i ‘2 i USER_DIR=$3’ install.sh
+sudo sed -i ‘2 i USER_AGENT_SERVER_IP=$4’ install.sh
+sudo sed -i ‘2 i USER_ENABLE_SYSCHECK=$5’ install.sh
+sudo sed -i ‘2 i USER_ENABLE_ROOTCHECK=$6’ install.sh
+sudo sed -i ‘2 i USER_ENABLE_ACTIVE_RESPONSE=$7’ install.sh
+sudo sed -i ‘2 i USER_ENABLE_SYSLOG=$8’ install.sh
+sudo echo -ne ‘\n’ | sudo sh install.sh en agent /var/ossec ${ossec_server_ip} y y y y
+sudo sed -i ‘12d’ /var/ossec/etc/ossec.conf
+sudo sed -i ‘12d’ /var/ossec/etc/ossec.conf
+sudo sed -i ’12 i <directories report_changes=”yes” check_all=”yes” realtime=”yes”>/etc,/usr,/var/log/</directories>’ /var/ossec/etc/ossec.conf
+sudo sed -i ’13 i <directories report_changes=”yes” check_all=”yes” realtime=”yes”>/bin,/sbin,/proc,/dev</directories>’ /var/ossec/etc/ossec.conf
+}
 
-
-OS=$(cat /etc/os-release | grep "^NAME" | awk -F'"' '{print $2}')
-version=$2
-
-
-if [[ $OS == "Ubuntu" ]]
-then
-
-    apt-get update  -y 
-    apt-get upgrade -y 
-    apt-get install build-essential make zlib1g-dev libpcre2-dev libevent-dev libssl-dev libz-dev -y
-
-    apt-get install apache2 php php7.0 apache2-utils unzip -y
-
-    cd /home/ubuntu/
-    wget https://github.com/ossec/ossec-hids/archive/refs/tags/3.1.0.tar.gz
-    tar -zxvf 3.1.0.tar.gz
-    cd ossec-hids-3.1.0
-    ./install.sh
-
-    /var/ossec/bin/ossec-control start
-
-    cd /home/ubuntu
-    wget https://github.com/ossec/ossec-wui/archive/master.zip
-    unzip master.zip
-
-    sudo mv ossec-wui-master /var/www/html/ossec
-    cd /var/www/html/ossec
-
-    /var/ossec/bin/ossec-control start
-    
-    systemctl restart apache2
-
-    else 
-        echo "your system is not supported"
-
-fi
+echo “Installing Ossec Agent”
+ossecAgent
